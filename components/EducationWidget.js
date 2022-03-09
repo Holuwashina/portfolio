9585f3a86cd4772b2e9c0670acd0929e8bbbf88d
Zhine <hamzatholuwashina@gmail.com>
2022-03-09 05:26:03 +0100

    Coding Night...

--- components/EducationWidget.js
+++ components/EducationWidget.js
@@ -24,28 +24,7 @@ const EduData = [
                 text: "B.Ed in Computer Science"
             },
         ]
-    },
-    {
-        id: 2,
-        contents: [
-            {
-                css: "subtitle1",
-                text: "University"
-            },
-            {
-                css: "caption",
-                text: "2017 - 2021"
-            },
-            {
-                css: "subtitle1",
-                text: "Tai Solarin University, Ijebu-Ode Nigeria."
-            },
-            {
-                css: "subtitle1",
-                text: "B.Ed in Computer Science"
-            },
-        ]
-    },
+    }
 ]
 
 
