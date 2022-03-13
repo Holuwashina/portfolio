44c6a01a93df3a837ddabf6a7f4157566bf5e9f5
Zhine <hamzatholuwashina@gmail.com>
2022-03-13 07:28:16 +0100

    Code Elegant...

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Squada+One&display=swap" rel="stylesheet" />
             </Head>
